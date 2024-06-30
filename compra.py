86964c6d3a769e654d603ccd7359ddd78652ffe4
avillaq <villafuertequispealex@gmail.com>
2024-06-30 00:39:16 -0500

    Refactor column names in Ventana class to include "Precio Venta"

--- compra.py
+++ compra.py
@@ -408,7 +408,7 @@ class Ventana(Frame):
         ###################################### MODIFICAR AQUI ###############################################
 
         # Modificar los nombres de las columnas en la lista nombre_columnas
-        nombre_columnas = ["Id Fiscal", "Marca", "Filtro", "Color", "Clase de Tratamiento", "Mentolado", "Fecha Compra", "Cantidad", "Precio"]
+        nombre_columnas = ["Id Fiscal", "Marca", "Filtro", "Color", "Clase de Tratamiento", "Mentolado", "Fecha Compra", "Cantidad", "Precio Venta"]
 
         ######################################################################################################
 
